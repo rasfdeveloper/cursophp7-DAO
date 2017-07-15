89c26de5357acff70bd405ee8975491f610d2191
Renato Franco <renatofr95@gmail.com>
2017-07-15 00:24:40 -0300

    Organizando o projeto

--- config.php
+++ config.php
@@ -2,7 +2,7 @@
 
 spl_autoload_register(function($class_name){
 
-	$file_name = $class_name .".php";
+	$file_name = "class" .DIRECTORY_SEPARATOR. $class_name .".php";
 
 	if(file_exists($file_name)){
 		require_once($file_name);
